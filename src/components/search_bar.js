6f24a9ef727b7cc7868aafe873dca0dba718f58b
vishuii <vishakhag6@gmail.com>
2017-08-04 12:56:21 +0530

    event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-  return <input/>
-};
+// const SearchBar = () => {
+//   return <input/>
+// };
+
+class SearchBar extends Component {
+  render() {
+    // return <input onChange={this.onInputChange} />;
+
+    // Error function can be write lie this
+    return <input onChange= {(event) => console.log(event.target.value)} />
+  }
+
+  // onInputChange(event){
+  //   console.log(event.target.value);
+  // }
+}
 
 export default SearchBar;
